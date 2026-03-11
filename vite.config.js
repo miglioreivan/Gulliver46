fe2641574bc0d1844f9e81842df6ba7402a27284
divanetto <miglioreivan2021@gmail.com>
2026-03-11 10:37:21 +0100

    config: set Vite base path to '/' unconditionally. (per Cloudflare Pages)

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/Gulliver46/' : '/',
+  base: '/',
   server: {
     port: 3000,
     open: true
